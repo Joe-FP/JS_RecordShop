5e955187f564ccc27fe0733f3a162514ada1a982
JoeFPollock <josephfpollock@gmail.com>
2018-07-29 11:03:09 +0100

    Added customer class and customer_spec

---
+++ customer.js
@@ -0,0 +1,15 @@
+const Customer = function(name){
+  this.name = name;
+  this.collection = [];
+  this.wallet = 50;
+}
+
+Record.prototype.buyRecord = function(){
+
+}
+
+Record.prototype.sellRecord = function(){
+
+}
+
+module.exports = Customer;
